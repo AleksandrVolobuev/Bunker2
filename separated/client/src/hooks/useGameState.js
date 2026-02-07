02784014e33d87949aaaa1d40d43277d34e53fd7
Alek <aleksandrvolobuev7676@gmail.com>
2026-02-07 16:21:26 +0200

    add README

--- separated/client/src/hooks/useGameState.js
+++ separated/client/src/hooks/useGameState.js
@@ -151,7 +151,7 @@ const useGameState = () => {
 
   // Инициализация сокетов и подписок.
   useEffect(() => {
-    const SERVER_URL = window.location.origin;
+    const SERVER_URL = import.meta.env.VITE_SERVER_URL || window.location.origin;
     const socket = io(SERVER_URL);
     socketRef.current = socket;
 
